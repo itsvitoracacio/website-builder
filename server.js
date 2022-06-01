d66f3d93162049ab884a863d3b8e8065ab120a9f
itsvitoracacio <vitor.acacio@pm.me>
2022-06-01 09:43:33 -0300

    Set a get request for the elements object and a start elements object containing 'typography' and 'buttons'

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const elements = {
+	typography: {
+		h1: {},
+		h2: {},
+		h3: {},
+		h4: {},
+		h5: {},
+		h6: {},
+		p: {},
+		a: {},
+		figcaption: {},
+	},
+	buttons: {
+		'.btn-primary': {},
+		'.btn-secondary': {},
+		'.btn-link-with-icon': {},
+	},
+}
+
 app.get('/', (_, res) => res.sendFile(__dirname + '/index.html'))
-app.get('/api/:param', (req, res) =>
-	res.json(`You queried: ${req.params.param}`)
-)
+
+// Read
+app.get('/api/pieces/elements/:type', (req, res) => {
+	const endpoint = req.params.type
+	res.json(elements[endpoint])
+})
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
