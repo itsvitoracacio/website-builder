6d5807dc823732da4e06c6e15aa16db722493b2e
itsvitoracacio <vitor.acacio@pm.me>
2022-06-01 08:43:05 -0300

    Added basic content to index.html and made changes to server.js so I can test that the server is working and the api endpoint is returning as it should.

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const PORT = 8000
 
-app.use(cors())
-
 app.get('/', (_, res) => res.sendFile(__dirname + '/index.html'))
-app.get('/api:param', (req, res) => res.json(`You queried ${req.params.param}`))
+app.get('/api/:param', (req, res) =>
+	res.json(`You queried: ${req.params.param}`)
+)
 
-app.listen(process.env.PORT || PORT, (_, _) => {
+app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
